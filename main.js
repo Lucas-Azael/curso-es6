ff11a33ce135bf864fde45bfe46123dd0e1270be
Lucas Soares <azaelsoares@gmail.com>
2019-08-26 21:16:24 -0300

    criando o bundle

---
+++ main.js
@@ -0,0 +1,30 @@
+class List {
+    constructor() {
+        this.data = [];
+    }
+    
+    add(data) {
+        this.data.push(data);
+        console.log(this.data); 
+    }
+}
+
+class TodoList extends List {
+    constructor() {
+        super(); //Interresante sempre que houver herança, a classe filha chamar a classe mãe
+
+        this.usuario = 'Lucas';
+    }
+
+    mostraUsuario() {
+        console.log(this.usuario);
+    }
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function() {
+    MinhaLista.add('Novo Todo');
+}
+
+MinhaLista.mostraUsuario();
